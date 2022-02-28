dbd13c56be058c24110d4ee4a8213fe48636d91b
rashedevs <rasheduap2015@gmail.com>
2022-03-01 04:18:44 +0600

    card created & result displayed

--- js/app.js
+++ js/app.js
@@ -14,6 +14,19 @@ const displayPhones = phones => {
     // console.log(phones)
     for (const phone of phones) {
         // console.log(phone)
-
+        const div = document.createElement('div')
+        div.classList.add('col-lg-4')
+        div.classList.add('my-5')
+        div.innerHTML = `
+ <div class="card" style="width: 25rem;">
+      <img src="${phone.image}" class="card-img-top w-75 mx-auto mt-2" alt="">
+      <div class="card-body">
+          <h5 class="card-title text-center">Name: ${phone.phone_name}</h5>
+          <p class="card-text text-center">Brand: ${phone.brand}</p>
+          <div class="text-center"><button onclick="" class="btn btn-primary w-50">See Details</button></div>
+      </div>
+</div>
+        `
+        main.appendChild(div)
     }
 }
