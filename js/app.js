3b44f71787ed6e604ebda320ffd4d4bc75de0b39
rashedevs <rasheduap2015@gmail.com>
2022-03-01 17:27:36 +0600

    input & result validation added

--- js/app.js
+++ js/app.js
@@ -1,22 +1,41 @@
+// main text fields--------------------------
 const main = document.getElementById('main')
 const box = document.getElementById('details-box')
+const error = document.getElementById('error')
+// load search data---------------------------
 const loadPhones = () => {
     const input = document.getElementById('input-value')
     const textKey = input.value
-    input.value = ''
-    const searchText = textKey.toLowerCase()
-    // const error = document.getElementById('error')
-    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayPhones(data.data))
+    // input validation-----------------------
+    if (input.value == '') {
+        main.textContent = ''
+        error.innerHTML = `Please enter a valid name or model.`
+    }
+    else {
+        input.value = ''
+        error.textContent = ''
+        //case convertion-----------------------
+        const searchText = textKey.toLowerCase()
+        const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                // result validation------------
+                if (data.data == '') {
+                    main.textContent = ''
+                    error.innerHTML = `No phone found.`
+                }
+                else { displayPhones(data.data) }
+            })
+    }
 }
 const displayPhones = phones => {
     // console.log(phones)
     main.textContent = ''
     box.textContent = ''
-    for (const phone of phones) {
-        // console.log(phone.slug)
+    const first20Data = phones.slice(0, 20)
+    for (const phone of first20Data) {
+        // console.log(phone)
         const div = document.createElement('div')
         div.classList.add('col-lg-4')
         div.classList.add('my-4')
@@ -43,7 +62,7 @@ const phoneDetails = id => {
         .then(data => setDetails(data.data))
 }
 const setDetails = details => {
-    console.log(details)
+    console.log(details.mainFeatures.sensors)
     box.textContent = ''
     const div = document.createElement('div')
     div.classList.add('my-4')
@@ -54,16 +73,18 @@ const setDetails = details => {
             <img id="detail-image" src="${details.image}" class="w-100" alt="">
             </div>
             <div class="col-lg-6">
-            <div class="card-body mt-2">
-                <h3 class="card-title">Name: ${details.name}</h3>
-                <h4 class="card-text">Release Date: ${details?.releaseDate}</h4>
-                <h5 class="card-text text-primary">Main Features:</h5>
+            <div class="card-body">
+                <h3 class="card-title"><span class="text-primary">Name:</span>  ${details.name}</h3>
+                <h5 class="card-text"><span class="text-primary">Release Date:</span> ${details?.releaseDate}</h5>
+                <h5 class="card-text text-primary">Features:</h5>
                 <p>Chipset: ${details.mainFeatures?.chipSet}</p>
                 <p>Storage: ${details.mainFeatures?.storage}</p>
                 <p>Memory: ${details.mainFeatures?.memory}</p>
                 <p>Display: ${details.mainFeatures?.displaySize}</p>
-                <h5 class="card-text text-primary">Sensor & Others:</h5>
-                <p>WLAN: ${details.others?.WLAN}, Bluetooth: ${details.others?.Bluetooth}, GPS: ${details.others?.GPS}, NFC: ${details.others?.NFC}</p>
+                <h5 class="card-text text-primary">Sensors:</h5>
+                <p>${details.mainFeatures.sensors}</p>
+                <h5 class="card-text text-primary">Others:</h5>
+                <p>WLAN: ${details.others?.WLAN}</p> <p>Bluetooth: ${details.others?.Bluetooth}</p> <p>GPS: ${details.others?.GPS}</p> <p>NFC: ${details.others?.NFC}</p>
             </div>
             </div>
         </div>
